01636219b644fa623552d619eadc8d03c4063130
YuYang <90436772+JadeGoat@users.noreply.github.com>
2025-09-03 15:05:39 +0800

    Create BookPage.jsx
    Add BookPage components

---
+++ frontend/src/BookPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BookPage = () => {
+  return (
+    <div>
+        <h2>Book management</h2>
+        <p>Here we will list all the books and manage books.</p>
+    </div>
+  )
+}
+
+export default BookPage
