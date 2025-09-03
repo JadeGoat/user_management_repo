c9417dd7e7cb3d2d567a7cfd90f247d89bf90f0f
YuYang <90436772+JadeGoat@users.noreply.github.com>
2025-09-03 15:07:26 +0800

    Update App.jsx
    Add BookPage components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import UserPage from './UserPage'
+import BookPage from './BookPage'
 
 function App() {
 
@@ -8,6 +9,8 @@ function App() {
       <div>
         <h1>User management application</h1>
         <UserPage/>
+        <hr/>
+        <BookPage/>
       </div>
     </>
   )
